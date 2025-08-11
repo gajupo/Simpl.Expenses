b75691c16eaea6faf440d4300b06a3d82bcb57e6
Gaudencio Jurado <gjurado@virtualidevs.com>
2025-08-10 21:28:07 -0600

    feat: Add OrderNumber column to ReimbursementDetails table

--- src/Domain/Entities/ReimbursementDetail.cs
+++ src/Domain/Entities/ReimbursementDetail.cs
@@ -7,5 +7,7 @@ namespace Simpl.Expenses.Domain.Entities
         public Report Report { get; set; }
         public string EmployeeName { get; set; }
         public string EmployeeNumber { get; set; }
+
+        public string? OrderNumber { get; set; }
     }
 }
---
+++ src/Infrastructure/Migrations/20250811032259_AddOrderNumberToReimbursementDetail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Simpl.Expenses.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderNumberToReimbursementDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderNumber",
+                table: "ReimbursementDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "ReimbursementDetails");
+        }
+    }
+}
